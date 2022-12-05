38d1280f65160d87d5776408207b92ee0add719b
David Dinh <daviddinh93@gmail.com>
2022-12-05 10:45:07 +0000

    day 5 complete

---
+++ 05b-solution.js
@@ -0,0 +1,41 @@
+const input = require("fs")
+  .readFileSync("./05-input.txt", "utf-8")
+  .split("\n\n");
+
+let stacks = [];
+
+input[0]
+  .split("\n")
+  .map(e => {
+    return e
+      .split("")
+      .map((_, j, arr) => {
+        if (j % 4 == 0) return [arr[j], arr[j + 1], arr[j + 2]];
+        return;
+      })
+      .filter(f => f);
+  })
+  .reverse()
+  .slice(1)
+  .map(e =>
+    e.map((f, j) => {
+      if (f[1] !== " ") {
+        stacks[j] ? stacks[j].push(f[1]) : stacks.push([f[1]]);
+      }
+    }),
+  );
+
+input[1]
+  .split("\n")
+  .map(e => e.split(" "))
+  .map(e => [e[1], e[3], e[5]])
+  .map(e => e.map(Number))
+  .map(e => {
+    tempStack = [];
+    for (let i = 0; i < e[0]; i++) {
+      tempStack.push(stacks[e[1] - 1].pop());
+    }
+    stacks[e[2] - 1].push(...tempStack.reverse());
+  });
+
+console.log(stacks.map(e => e.pop()).join(""));
